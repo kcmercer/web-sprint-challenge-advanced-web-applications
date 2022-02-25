9e8309f67cf39f49cdc397aeeca82a43b29268b1
Kenneth Mercer <kmercer5439@gmail.com>
2022-02-25 14:05:32 -0600

    doing some thinking.

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -9,6 +9,12 @@ test('sanity', () => {
   expect(true).toBe(true)
 })
 
+// This seems too simple, but I don't see any other props that Spinner can take
+// beyond a boolean. In the Sprint Assessment it referances using a Get request, but
+// I don't see why/how I would test that. The spinner just flips on or off.
+
+// I'm going to give it some thought and come back to this.
+
 test('renders without errors', () => {
   render(<Spinner />)
 })
